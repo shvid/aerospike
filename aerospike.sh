da4b807cbabadf7b2dcf450b894da8fb482bd153
AlexShvid <a@shvid.com>
2020-01-16 19:13:26 -0800

    first commit

---
+++ aerospike.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+BIN=`pwd`
+
+echo "Start Aerospike from $BIN."
+echo "1. Interactive"
+echo "2. Background"
+
+read -p 'Select mode [N]:' mode
+
+FLAG=-it
+
+if [[ "$mode" == "1" ]]; then
+   FLAG=-it
+elif [[ "$mode" == "2" ]]; then
+   FLAG=-d
+else
+   echo "Wrong number"
+   exit
+fi
+
+docker run $FLAG --rm -v "$BIN:/opt/aerospike" --name aerospike -p 3000:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 aerospike asd --foreground --config-file /opt/aerospike/etc/aerospike.conf
---
+++ smd/README.md
@@ -0,0 +1,1 @@
+# system metadata dir
